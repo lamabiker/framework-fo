01007d5a5fffe5598195ac142f9535f46d91bcee
jbpham <jbpham@kernix.com>
2015-11-18 17:59:46 +0100

    add style guide + maj less

---
+++ templates/pages/style-guide.php
@@ -0,0 +1,99 @@
+<!-- style guide -->
+<div class="header-wrap-style-guide">
+	<h2>Style Guide</h2>
+</div>
+
+<div class="main-wrap-style-guide">
+	<div class="container">
+		<div class="row">
+			<div class="style-guide-row">
+				<div class="col-sm-6">
+					<h3>Button</h3>
+					<button type="" class="btn">btn</button>
+					<button type="" class="btn btn-primary">btn btn-primary </button>
+					<button type="" class="btn btn-secondary">btn btn-secondary </button>
+					<button type="" class="btn btn-tertiary">btn btn-tertiary </button>
+				</div>
+
+				<div class="col-sm-6">
+					<h3>Button link</h3>
+					<a href="" class="link">btn</a>
+					<a href="" class="btn">btn</a>
+					<a href="" class="btn btn-primary">btn btn-primary </a>
+					<a href="" class="btn btn-secondary">btn btn-secondary </a>
+					<a href="" class="btn btn-tertiary">btn btn-tertiary </a>
+				</div>
+			</div>
+
+			<div class="style-guide-row">
+				<div class="col-sm-6">
+					<h3>Input</h3>
+					<div>	.form-control</div>
+					<input type="text" name="" value="" class="form-control" placeholder="input text">
+					<h3>Textarea</h3>
+					<div>	.form-control</div>
+					<textarea name="" class="form-control"></textarea>
+				</div>
+
+				<div class="col-sm-6">
+					<h3>Checkboxes and radios</h3>
+					<form >
+						<div class="form-radio">
+							<input type="radio" name="radio[]" value="" id="radio">
+							<label for="radio"><span class="icon"></span> Titre </label>
+						</div>
+						<div class="form-radio">
+							<input type="radio" name="radio[]" value="" id="radio1">
+							<label for="radio1"><span class="icon"></span> Titre 1</label>
+						</div>
+					</form>
+					<form >
+						<div class="form-checkbox">
+							<input type="checkbox" name="" value="" placeholder="" id="checkbox">
+							<label for="checkbox"><span class="icon"></span> Titre </label>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="style-guide-row">
+				<div class="col-sm-6">
+					<h3>Select</h3>
+					<div class="select">
+
+					</div>
+					<select name="" class="selectpicker">
+						<option value="">Text</option>
+						<option value="">Text</option>
+						<option value="">Text</option>
+						<option value="">Text</option>
+
+					</select>
+				</div>
+				<div class="col-sm-6">
+					<h3>Formulaire</h3>
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+						<div class="form-group">
+							<label for="exampleInputFile">File input</label>
+							<input type="file" id="exampleInputFile">
+							<p class="help-block">Example block-level help text here.</p>
+						</div>
+						<div class="checkbox">
+							<label>
+								<input type="checkbox"> Check me out
+							</label>
+						</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
