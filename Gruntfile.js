47ee57cd322439a3200b3ca8d48511f1a3ff82ee
Liam <lbaker@kernix.com>
2015-11-20 15:52:37 +0100

    Add .gitignore Amend gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,34 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        less: {
-            development: {
-                files: {
-                    'css/theme.css': 'src/theme/less/theme.less'
-                },
-                options: {
-                  sourceMap: true,
-                  sourceMapFilename: '/css/theme.css.map',
-                  sourceMapRootpath: '/src/theme/'
-                }
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        files: {
+          'css/theme.css': 'src/theme/less/theme.less'
         },
-        watch: {
-            src: {
-                files: ['src/theme/**'],
-                tasks: ['all']
-            }
+        options: {
+          sourceMap: true,
+          sourceMapFilename: 'css/theme.css.map',
+          sourceMapURL: '/css/theme.css.map'
         }
-    });
+      }
+    },
+    watch: {
+      src: {
+        files: ['src/theme/less/**'],
+        tasks: ['all']
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    
-    grunt.registerTask('all', ['less']);
-    
-    grunt.registerTask('default', function () {
-        grunt.log.writeln("\nUsage:");
-        grunt.log.writeln(" * grunt all");
-        grunt.log.writeln(" * grunt watch");
-    });
-    
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('all', ['less']);
+
+  grunt.registerTask('default', function () {
+    grunt.log.writeln("\nUsage:");
+    grunt.log.writeln(" * grunt all");
+    grunt.log.writeln(" * grunt watch");
+  });
 };
